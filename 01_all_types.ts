d3d4c38f11f13f1d5780850c464d1601140841f3
Subhranil Sarkar <subhranil2605@gmail.com>
2023-04-08 13:11:39 +0530

    All types of the Typescript

---
+++ 01_all_types.ts
@@ -0,0 +1,30 @@
+// for any number; like 10, 3.1541
+let a: number;
+
+// for true and false
+let b: boolean;
+
+// for string values
+let c: string;
+
+// any type
+let d: any;
+
+// Array
+let e: number[] = [1, 2, 3];
+
+// any array
+let f: any[] = [1, true, "a", false];
+
+// enum
+
+// const coloRed = 0;
+// const coloGreen = 1;
+// const coloBlue = 2;
+
+enum Color {
+  Red = 0,
+  Green = 1,
+  Blue = 2,
+}
+let backgroundColor = Color.Blue;
